a6922a6ff74bf3c0e746236e7a321e595df0f014
Ahamed4129 <123795712+Ahamed4129@users.noreply.github.com>
2024-08-22 09:59:06 +0530

    Add files via upload

---
+++ app/account/constants.ts
@@ -0,0 +1,6 @@
+export class Constants {
+  public static apiRoot = "http://localhost:9010";
+  public static clientRoot = "http://localhost:4200";
+  public static idpAuthority = "https://localhost:9009"
+  public static clientId = "angular-client";
+}
